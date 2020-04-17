d6031261c4220a2da8e3f77e7a999cf4ea17a150
Darwin Escalona <darwin.escalona@carsales.com>
2020-04-17 09:33:52 -0400

    Add events

--- src/app/dog-of-the-day/dog-of-the-day.component.ts
+++ src/app/dog-of-the-day/dog-of-the-day.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input, Output, EventEmitter, ChangeDetectorRef } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'ng-chile-dog',
@@ -8,8 +10,11 @@ import { HttpClient } from '@angular/common/http';
     <link href="https://fonts.googleapis.com/css?family=Nanum+Pen+Script" rel="stylesheet">
     <div class="component_container">
       <h1>{{message}}</h1>
-      <img src="{{imagen}}" *ngIf="imagen"/>
+      <img src="{{imagen}}" />
     </div>
+    <br>
+    <button *ngIf="imagen.length > 0" (click)="sendUrl()">Emitir URL</button>
+    <br>
   `,
   styles: [`
     .component_container {
@@ -30,17 +35,49 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DogOfTheDayComponent implements OnInit {
 
-  public imagen;
-  public message = 'Dog of The Day';
+  public imagen = '';
+  @Input() message = 'Dog of The Day';
+  @Output() imagenEvent = new EventEmitter<string[]>();
+
+  private refresh$ = new BehaviorSubject<boolean>(false);
+
+  // change refresh to use getter and setter
+  @Input()
+  set refresh(value) {
+      // set the latest value for  BehaviorSubject
+      this.refresh$.next(!this.refresh);
+  }
+  get refresh() {
+      // get the latest value from  BehaviorSubject
+      return this.refresh$.getValue();
+  }
 
   constructor(
-    public http: HttpClient
+    public http: HttpClient, private ref: ChangeDetectorRef
   ) { }
 
   ngOnInit() {
+    this.refresh$
+            .subscribe(x => {
+              console.log('refresh', x);
+              this.loadImage();
+            });
+    this.loadImage();
+  }
+
+  loadImage() {
     this.http.get(environment.api_url).subscribe((dotd: any) => {
       this.imagen = dotd.message;
     });
+  }
+
+  sendUrl() {
+    console.log('sendUrl');
+    this.imagenEvent.emit([this.imagen, 'prueba']);
+  }
+
+  getUrl() {
+    return this.imagen;
   }
 
 }
